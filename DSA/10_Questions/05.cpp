f5bed59943fd0c26069daf0f85602a61054eb259
Chhavi <gusainchhavi@gmail.com>
2025-02-18 01:01:44 +0530

    add file

---
+++ DSA/10_Questions/05.cpp
@@ -0,0 +1,35 @@
+/*C++ solution that efficiently answers queries about the frequency of a given character in the string. The approach preprocesses the frequency of each character in the string and answers each query in constant time 
+O(1).*/
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+void preprocessFrequency(const string& str, unordered_map<char, int>& freq){
+    for (char ch : str){
+        freq[ch]++;
+    }
+}
+
+int main(){
+    string str = "abacabde"; // Given string
+    int q;
+    cout << "Enter number of queries: ";
+    cin >> q;
+
+    unordered_map<char, int> freq;
+    preprocessFrequency(str, freq);
+
+    while (q--){
+        char ch;
+        cout << "Enter character to find: ";
+        cin >> ch;
+
+        if (freq.find(ch) != freq.end()){
+            cout << "Frequency of '" << ch << "' is: " << freq[ch] << endl;
+        } else{
+            cout << "Character '" << ch << "' not found in the string." << endl;
+        }
+    }
+
+    return 0;
+}
